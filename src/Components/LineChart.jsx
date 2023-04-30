fd7b0d2beeaadc167dcd423fd7ae50f0ff39ac50
RakaMurdiarta <dewamurdiarta@gmail.com>
2023-05-01 02:59:19 +0800

    progress 1 may

--- src/Components/LineChart.jsx
+++ src/Components/LineChart.jsx
@@ -10,6 +10,8 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+import { useUserContext } from "../context/userContext";
+import { UseProducts } from "../context/productContext";
 
 ChartJS.register(
   CategoryScale,
@@ -21,41 +23,156 @@ ChartJS.register(
   Legend
 );
 const LineChart = () => {
+  const { users } = useUserContext();
+  const { products } = UseProducts();
+
+  const month = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+  const handleFilter = (month) => {
+    const filter = users.filter((f) => {
+      return new Date(f.createdAt).getMonth() + 1 === month;
+      // return f.title === "Smartwatch black metal";
+    });
+    return filter;
+  };
+
+  const dataMaping = users.map((p) => {
+    return {
+      x: month[new Date(p.createdAt).getMonth()],
+      y: handleFilter(new Date(p.createdAt).getMonth() + 1).length,
+    };
+  });
+
+  const data2 = dataMaping // cara mendapatkan nilai unik tanpa agregat
+    .filter(
+      (item, index, self) => self.findIndex((t) => t.x === item.x) === index
+    )
+    .map((item) => ({ x: item.x, y: item.y }));
+
+  const dataMapingUser = data2.sort((a, b) => {
+    const aMonth = month.indexOf(a.x);
+    const bMonth = month.indexOf(b.x);
+    return aMonth - bMonth;
+  });
+
   const options = {
+    responsiveAnimationDuration: 1000,
+    animations: {
+      duration: 2000,
+      easing: "linear",
+      from: 3,
+      to: 0,
+    },
     responsive: true,
+    interaction: {
+      mode: "nearest",
+      axis: "x",
+      intersect: false,
+      events: ["mousemove", "mouseout", "click", "touchstart", "touchmove"],
+    },
     plugins: {
       legend: {
-        position: "top",
+        position: "chartArea",
+        display: false,
       },
       title: {
-        display: true,
+        display: false,
         text: "Chart.js Line Chart",
+      },
+      tooltip: {
+        backgroundColor: "rgba(47, 40, 42, 0.8)",
+        usePointStyle: true,
+        padding: 10,
+        bodyFont: {
+          size: 10,
+          width: 100,
+        },
+        callbacks: {
+          label: (context) => {
+            return "user" + ": " + context.parsed.y;
+          },
+        },
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        border: {
+          color: "white",
+        },
+        ticks: {
+          padding: 4,
+          font: {
+            size: 15,
+          },
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+        min: 0,
+        ticks: {
+          beginAtZero: true,
+          font: {
+            size: 15,
+          },
+          padding: 10,
+        },
+        border: {
+          color: "white",
+        },
       },
     },
   };
 
   const labels = [
-    "January",
-    "February",
-    "March",
+    "Januari",
+    "Februari",
+    "Maret",
     "April",
-    "May",
-    "June",
-    "July",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
   ];
 
   const data = {
     labels,
     datasets: [
       {
-        label: "Example",
-        data: [10, 2, 3, 14, 95, 16, 27],
-        borderColor: "rgb(255, 99, 132)",
+        label: "Data Set 1",
+        data: dataMapingUser,
+        borderColor: "rgba(42, 36, 37, 0.8)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
+        pointStyle: "circle",
+        pointRadius: 6, // ukuran symbol
+        pointBackgroundColor: "white", // warna symbol
+        pointBorderColor: "rgba(42, 36, 37, 0.8)", // warna border symbol
+        pointBorderWidth: 2, // lebar border symbol
+        tension: 0.1,
       },
     ],
   };
-  return <Line data={data} options={options} />;
+  return <Line data={data} options={options} height={"80%"} />;
 };
 
 export default LineChart;
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -11,30 +11,36 @@ const Dashboard = () => {
 
   return (
     <section>
-      <div className="grid grid-cols-3 gap-3 text-white">
-        <div className="p-5 bg-yellow-500 rounded-xl shadow-lg">
-          <div className="text-xl font-extrabold uppercase">Users</div>
-          <div className="flex justify-between items-center ml-3 mt-3">
-            <FaUsers size={60} />
-            <div className="font-extrabold text-2xl">{`${users.length} active`}</div>
+      <div className="grid grid-cols-4 gap-2 ">
+        <div className="col-span-3">
+          <div className="grid grid-cols-2 gap-3 text-white">
+            <div className="p-5 bg-yellow-500 rounded-xl shadow-lg">
+              <div className="text-xl font-extrabold uppercase">Users</div>
+              <div className="flex justify-between items-center ml-3 mt-3">
+                <FaUsers size={60} />
+                <div className="font-extrabold text-2xl">{`${users.length} active`}</div>
+              </div>
+            </div>
+            <div className="p-5 rounded-xl bg-indigo-700 shadow-xl">
+              <div className="text-xl font-extrabold uppercase">Products</div>
+              <div className="flex justify-between items-center ml-3 mt-3">
+                <RiShoppingCartFill size={60} />
+                <div className="font-extrabold text-2xl">{`${products.length} active`}</div>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-3 my-7 gap-6">
+            <div className="col-span-3 bg-green-300 text-white p-6 rounded-xl shadow-xl">
+              <div className="mb-6 text-lg font-semibold">User Growth</div>
+              <LineChart />
+            </div>
+            <div className="col-span-3 bg-gray-300 text-stone-800 p-6 rounded-xl shadow-xl">
+              <div className="mb-6 text-lg font-semibold">User Growth</div>
+              <LineChart />
+            </div>
           </div>
         </div>
-        <div className="p-5 border rounded-xl bg-indigo-700 shadow-xl">
-          <div className="text-xl font-extrabold uppercase">Products</div>
-          <div className="flex justify-between items-center ml-3 mt-3">
-            <RiShoppingCartFill size={60} />
-            <div className="font-extrabold text-2xl">{`${products.length} active`}</div>
-          </div>
-        </div>
-      </div>
-      <div className="grid grid-cols-3 my-10 gap-2">
-        <div className="p-3 border"></div>
-        <div className="border">
-          <LineChart />
-        </div>
-        <div className="border">
-          <LineChart />
-        </div>
+        <div className="border col-span-1"> fajfkaf</div>
       </div>
     </section>
   );
